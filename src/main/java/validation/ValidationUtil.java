c2338db57ef4b002374ac29679208b3a2992e034
Yevhen <ibookmack@gmail.com>
2019-03-28 11:49:16 +0200

    fixed const

--- src/main/java/validation/ValidationUtil.java
+++ src/main/java/validation/ValidationUtil.java
@@ -4,10 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationUtil {
-    private final String PHONE_NUMBER = "^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$";
-    private final String EMAIL = "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
-    private final String DATE = "[0-9]{4}-(0[1-9]|1[0-12])-(0[1-9]|1[0-9]|2[0-9]|3[01])";
-    private final String IP_ADRESS = "((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)";
+    private static final String PHONE_NUMBER = "^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$";
+    private static final String EMAIL = "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
+    private static final String DATE = "[0-9]{4}-(0[1-9]|1[0-12])-(0[1-9]|1[0-9]|2[0-9]|3[01])";
+    private static final String IP_ADRESS = "((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)";
 
     public boolean phoneValidation(String phone) {
         Pattern pattern = Pattern.compile(PHONE_NUMBER);
